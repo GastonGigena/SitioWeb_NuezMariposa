75c157c568de6471af14ffcbfdf7f2569c32e921
GastonGigena <liongigena@gmail.com>
2025-05-30 12:05:12 -0300

    Estructura otras paginas y css

--- Js/login.js
+++ Js/login.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loginFormAside = document.getElementById("loginFormAside");
-    if (loginFormAside) {
-        loginFormAside.addEventListener("submit", function (e) {
-            e.preventDefault();
-            const nombreUsuario = document.getElementById("usuario").value;
-            alert("¡Hola, " + nombreUsuario + "! Gracias por ingresar a nuestro sitio");
-            window.location.reload();
-        });
-    }
+  const loginFormAside = document.getElementById("loginFormAside");
+  if (loginFormAside) {
+    loginFormAside.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const nombreUsuario = document.getElementById("usuario").value;
+      alert(
+        "¡Hola, " + nombreUsuario + "! Gracias por ingresar a nuestro sitio"
+      );
+      window.location.reload();
+    });
+  }
 });
+
+let indice = 0;
+
+function moverCarrusel(direccion) {
+  const carrusel = document.getElementById("carrusel");
+  const total = carrusel.children.length;
+  indice = (indice + direccion + total) % total;
+  carrusel.style.transform = `translateX(-${indice * 100}%)`;
+}
+
